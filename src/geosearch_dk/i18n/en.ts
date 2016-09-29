bbfbce95a665d2f4d305c2dc106c3d20af86c322
Frederikssund Kommune <gis@frederikssund.dk>
2016-09-29 14:18:49 +0200

    Finalizing translations

--- src/geosearch_dk/i18n/en.ts
+++ src/geosearch_dk/i18n/en.ts
@@ -106,21 +106,21 @@
         <translation>About</translation>
     </message>
 </context>
-<context>
+<context encoding="UTF-8">
     <name>SearchBox</name>
     <message encoding="UTF-8">
-        <location filename="searchbox.py" line="298"/>
+        <location filename="searchbox.py" line="383"/>
         <source>Geosearch DK lader brugeren zoome til navngivne steder i Danmark.&lt;br /&gt;Pluginet benytter tjenesten &apos;geosearch&apos; fra &lt;a href=&quot;http://kortforsyningen.dk/&quot;&gt;kortforsyningen.dk&lt;/a&gt; og kræver derfor et gyldigt login til denne tjeneste.&lt;br /&gt;Pluginets webside: &lt;a href=&quot;http://github.com/Septima/qgis-geosearch&quot;&gt;github.com/Septima/qgis-geosearch&lt;/a&gt;&lt;br /&gt;Udviklet af: Septima&lt;br /&gt;Mail: &lt;a href=&quot;mailto:kontakt@septima.dk&quot;&gt;kontakt@septima.dk&lt;/a&gt;&lt;br /&gt;Web: &lt;a href=&quot;http://www.septima.dk&quot;&gt;www.septima.dk&lt;/a&gt;
 </source>
         <translation>Geosearch DK lets the user zoom to named places in Denmark.&lt;br /&gt;The plugin uses the web service &apos;geosearch&apos; by &lt;a href=&quot;http://kortforsyningen.dk/&quot;&gt;kortforsyningen.dk&lt;/a&gt;. A valid login for this service is therefore needed.&lt;br /&gt;Plugin web site: &lt;a href=&quot;http://github.com/Septima/qgis-geosearch&quot;&gt;github.com/Septima/qgis-geosearch&lt;/a&gt;&lt;br /&gt;Made by: Septima&lt;br /&gt;Mail: &lt;a href=&quot;mailto:kontakt@septima.dk&quot;&gt;kontakt@septima.dk&lt;/a&gt;&lt;br /&gt;Web: &lt;a href=&quot;http://www.septima.dk&quot;&gt;www.septima.dk&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="searchbox.py" line="150"/>
+        <location filename="searchbox.py" line="225"/>
         <source>Bruger afvist af Kortforsyningen</source>
         <translation>User rejected by Kortforsyningen</translation>
     </message>
     <message>
-        <location filename="searchbox.py" line="151"/>
+        <location filename="searchbox.py" line="226"/>
         <source>Manglende eller ukorrekt brugernavn og password til Kortforsyningen.
 
 Kortforsyningen svarede:
@@ -131,14 +131,59 @@ Kortforsyningen responded:
 </translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="searchbox.py" line="76"/>
+        <location filename="searchbox.py" line="77"/>
         <source>Søg adresse, stednavn, postnummer, matrikel m.m.</source>
         <translation>Search Danish addresses, road names, place names, postal codes, land registry etc.</translation>
     </message>
     <message>
-        <location filename="searchbox.py" line="173"/>
+        <location filename="searchbox.py" line="248"/>
         <source>Ingen resultater</source>
         <translation>No results</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="searchbox.py" line="134"/>
+        <source>  Værdi for stregtykkelse til valgboks er ulovlig (1 - 50); sættes til standard: 4</source>
+        <translation>  Width value for rubberband is out of range (1 - 50); defaulting to 4</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="searchbox.py" line="138"/>
+        <source>  Værdi af linietype til valgboks er ulovlig (0 - 5); sættes til standard: 1, simpel linie</source>
+        <translation>  Style value for rubberband is out of range (0 - 5); defaulting to 1, simple line</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="searchbox.py" line="142"/>
+        <source>  Buffer værdi for valgboks er ulovlig (0 - 10000); sættes til standard: 200.0</source>
+        <translation>  Buffer value for rubberband is out of range (0 - 10000); defaulting to 200.0</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="searchbox.py" line="146"/>
+        <source>  Farveværdi for valgmarkør er ulovlig - sættes til standard farve: rød</source>
+        <translation>  Color for marker is out of range; defaulting to color red</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="searchbox.py" line="150"/>
+        <source>  Værdi for stregtykkelse til valgmarkør er ulovlig (1 - 50); sættes til standard: 4</source>
+        <translation>  Width value for marker is out of range (1 - 50); defaulting to 4</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="searchbox.py" line="154"/>
+        <source>  Værdi for størrelse af valgmarkør er ulovlig (10 - 200); sættes til standard: 30</source>
+        <translation>  Size value for marker is out of range (10 - 200); defaulting to 30</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="searchbox.py" line="158"/>
+        <source>  Symbolværdi for valgmarkør er ulovlig (0 - 4); sættes til standard: 1, kryds</source>
+        <translation>  Icon value for marker is out of range (0 - 4); defaulting to 1, cross</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="searchbox.py" line="162"/>
+        <source>  Buffer værdi for valgmarkør er ulovlig (20 - 10000); sættes til standard: 200.0</source>
+        <translation>  Buffer value for marker is out of range (20 - 10000); defaulting to 200.0</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="searchbox.py" line="130"/>
+        <source>  Farveværdi for valgboks er ulovlig - sættes til standard farve: rød</source>
+        <translation>  Color for rubberband is out of range; defaulting to color red</translation>
     </message>
 </context>
 <context>
@@ -146,7 +191,8 @@ Kortforsyningen responded:
     <message>
         <location filename="ui_search.ui" line="20"/>
         <source>Search</source>
-        <translation>Search</translation>
+        <translation>Søg</translation>
     </message>
 </context>
+
 </TS>
